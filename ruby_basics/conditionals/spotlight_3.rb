3af02bdade3aa7d13574525e59552c5c3ed4f7a1
Will Rossen <wor101@gmail.com>
2020-09-25 19:48:04 +0000

    Adding conditional excercises

---
+++ ruby_basics/conditionals/spotlight_3.rb
@@ -0,0 +1,11 @@
+# spotlight_3.rb
+
+# reformat following case statement so it only takes up 5 lines
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green' then puts 'Go!'
+when 'yellow' then  puts 'Slow down!'
+else puts 'Stop!'
+end

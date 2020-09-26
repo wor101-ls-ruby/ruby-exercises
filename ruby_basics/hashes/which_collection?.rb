0c71674dbd84685ccac61ee7da4c0f3412985634
Will <wor101@gmail.com>
2020-09-26 17:21:28 -0400

    Adding debugging excercises

--- ruby_basics/hashes/which_collection?.rb
+++ ruby_basics/hashes/which_collection?.rb
@@ -2,6 +2,6 @@
 
 # rewrite car as a nested array containing the same key-value pairs
 
-car = [[type:  'sedan'], [color: 'blue'], [year:  2003]]
+car = [[type:  'sedan'], [color: 'blue'], [year:  2003]]      # Arrays are preferred for ordered data
 
 p car

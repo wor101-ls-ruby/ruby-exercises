0c19f76397f2e966f0c919b5288300da2ca14461
Will <wor101@gmail.com>
2021-02-18 21:57:58 +0000

    last of medium2 testing

--- RB130/medium2_testing/transaction_test.rb
+++ RB130/medium2_testing/transaction_test.rb
@@ -7,7 +7,8 @@ class TransactionTest < MiniTest::Test
   def test_prompt_for_payment
     transaction = Transaction.new(15)
     input = StringIO.new("30\n")
-    transaction.prompt_for_payment(input: input)
+    output = StringIO.new
+    transaction.prompt_for_payment(input: input, output: output)
     assert_equal(30, transaction.amount_paid)
   end
 end
